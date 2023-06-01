9863ee66060e7ce916f3353d47dd9f733aebb749
Martin Vicente <martin.vicente@outlook.com>
2023-06-01 01:09:39 -0300

    Funciones agregadas

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-/*
-Capturar entradas por prompt
-Declarar variables y objetos para simular el proceso
-Crear funciones y/o métodos para realizar operaciones (suma, resta, %, etc)
-Efectuar salida por alert
-
-Se debe entregar
-- HTML
-- Variables JS
-- Funciones esenciales del proyecto
-- Objetos JS
-- Arrays
-- Métodos de búsqueda y filtrado sobre el array
-*/
-
+// Creación clase producto
 class Producto{
     constructor(id, nombre, precio, seccion){
         this.id = id;
@@ -22,6 +8,7 @@ class Producto{
     }
 }
 
+//Creación objetos productos y push al array
 const productos = [];
 const remera = new Producto(1, "Remera", 1500, "Indumentaria");
 const camisa = new Producto(2, "Camisa", 2300, "Indumentaria");
@@ -35,7 +22,18 @@ const pulsera = new Producto(9, "Pulsera", 900, "Joyería");
 
 productos.push(remera, camisa, vestido, notebook, celular, computadora, aros, collar, pulsera);
 
+//Funciones calculo
+function precioEfectivo(precio, cantidad, descuento){
+    let total = precio * cantidad * descuento;
+    return total;
+}
 
+function precioTarjeta(precio, cantidad){
+    let total = precio * cantidad;
+    return total;
+}
+
+// Prompts y alerts
 let seccion = prompt("Bienvenido! Por favor, ingrese el número de la sección a la que desea ingresar: 1- Indumentaria, 2-Electrónica, 3-Joyería");
 
 if(seccion == 1){
@@ -43,44 +41,38 @@ if(seccion == 1){
     indumentaria.forEach(item =>
         alert(`Item Nº${item.id}, Producto: ${item.nombre}, Precio: $${item.precio}`)
     )
-    
+
     let producto = prompt("Seleccione el número de producto que desea comprar");    
     let cantidad = prompt("Ingrese la cantidad que desea");
     let medioDePago = prompt("Ingrese el medio de pago: 1- Efectivo (10% dto), 2- Tarjeta");
 
     if(producto == 1){
         let precio = remera.precio;        
-        if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;           
-            alert(`El total de su compra es de $${total}`)
+        if(medioDePago == 1){      
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }        
     }else if(producto == 2){
         let precio = camisa.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;            
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }
     }else{
         let precio = vestido.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;            
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }
     }
 }else if(seccion == 2){
     let electronica = productos.filter((item) => item.seccion === "Electrónica");
     electronica.forEach(item =>
         alert(`Item Nº${item.id}, Producto: ${item.nombre}, Precio: $${item.precio}`)
-    )
+    );
 
     let producto = prompt("Seleccione el número de producto que desea comprar:");
     let cantidad = prompt("Ingrese la cantidad que desea");
@@ -89,36 +81,30 @@ if(seccion == 1){
     if(producto == 4){
         let precio = notebook.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;           
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }        
     }else if(producto == 5){
         let precio = celular.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;            
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }
     }else{
         let precio = computadora.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;           
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }
     }
 }else{
     let joyeria = productos.filter((item) => item.seccion === "Joyería");
     joyeria.forEach(item =>
         alert(`Item Nº${item.id}, Producto: ${item.nombre}, Precio: $${item.precio}`)
-    )
+    );
 
     let producto = prompt("Seleccione el número de producto que desea comprar:");
     let cantidad = prompt("Ingrese la cantidad que desea");
@@ -127,29 +113,23 @@ if(seccion == 1){
     if(producto == 7){
         let precio = aros.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;           
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }        
     }else if(producto == 8){
         let precio = collar.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;            
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }
     }else{
         let precio = pulsera.precio;        
         if(medioDePago == 1){
-            let total = precio * cantidad * 0.9;            
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioEfectivo(precio, cantidad, 0.9)}`);
         }else{
-            let total = precio * cantidad;
-            alert(`El total de su compra es de $${total}`)
+            alert(`El total de su compra es de $${precioTarjeta(precio, cantidad)}`);
         }
     }
 }
